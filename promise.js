c6c4fb00e9f3f163b03416f1dca60a28f4aca663
fazrisuhada007 <fazrisuhada3@gmail.com>
2022-09-17 13:24:54 +0900

    jumlah emosi

--- promise.js
+++ promise.js
@@ -1,7 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  await new Promise((r) => setTimeout(r, 200));
+
+  return new Promise((resolve) => {
+    let countEmosi = 0;
+
+    promiseTheaterIXX()
+      .then(
+        (resultPromiseTheaterIXX) => {
+          resultPromiseTheaterIXX.forEach(function (value) {
+            if (value.hasil == emosi) {
+              ++countEmosi;
+            }
+          });
+          promiseTheaterVGC()
+            .then(
+              (resultPromiseTheaterVGC) => {
+                resultPromiseTheaterVGC.forEach(function (value) {
+                  if (value.hasil == emosi) {
+                    ++countEmosi;
+                  }
+                });
+                resolve(countEmosi);
+              }
+            );
+        }
+      );
+  });
+};
 
 module.exports = {
   promiseOutput,
